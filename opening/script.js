4414b58962b2bee27dbbb66e889f5fa9d7ad19c1
トレノケート <75574048+TRAINOCAMP@users.noreply.github.com>
2021-11-18 19:18:16 +0900

    [New] Add new files.

---
+++ opening/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+//alert("これはalertです。")
+
+// 非推奨
+//document.write('Hello World');
+//document.write('<p>Hello World</p>');
+
+let element = document.currentScript;
+element.insertAdjacentHTML('beforebegin', '<p>Hello</p>');
+// 'beforebegin'  element の前に追加
+// 'afterbegin'   element の子要素の先頭に追加
+// 'beforeend'    element の子要素の最後に追加
+// 'afterend'     element の後に追加
